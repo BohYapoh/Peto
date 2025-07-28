acc2eb3f7c3e5f388ebc03430d680378472feec3
BohYapoh <seregin.petr0705@gmail.com>
2025-07-28 20:24:32 +0300

    Gosling

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import numpy as np
 import time
 import PIL
 import pygame
+import arcade
+import random
 from PIL import Image, ImageTk
+from pygame.transform import rotate
+
 print("Hello world")
 
 v_0 = 0
 y_0 = 500
-g = 9.81
+g = 19.81
 C = 0.0022
 dt = 0.01
 
-t = np.linspace(0, 20, 2001)
+t = np.linspace(0, 30, 1801)
 y = np.zeros((len(t)))
 v = np.zeros((len(t)))
 a = np.zeros((len(t)))
@@ -29,135 +33,144 @@ k_2 = 0
 k_3 = 0
 k_4 = 0
 
-class Svin:
+gosling_time = 0
+
+
+class GG (arcade.Window):
+
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.background = arcade.Sprite("Landscape.png", scale = 0.78125)
+        self.background.center_x = 600
+        self.background.center_y = 400
+        self.sprite_list = arcade.SpriteList()
+        self.sprite_list.append(self.background)
+        self.car = Svin()
+        self.score = 0
+        self.coin = Gosling(0)
+        self.num = 1
+        self.coin_list = []
+        self.coin_list.append(self.coin)
+        self.sprite_list.append(self.car)
+        self.sprite_list.append(self.coin)
+
+    def on_key_press(self, key, modifiers):
+        self.car.on_key_press(key)
+
+    def on_key_release(self, key, modifiers):
+        self.car.on_key_release(key)
+
+    def on_draw(self):
+        #print(type(self.background))
+        self.sprite_list.draw()
+        arcade.draw_text(self.score, x = 100, y =200 )
+
+    def on_update(self, delta_time):
+        global C
+        global g
+        global gosling_time
+        gosling_time += 1
+        self.car.update()
+        #self.coin.update()
+        for item in self.coin_list:
+            item.move_coin()
+            if arcade.check_for_collision(self.car, item):
+                if item.Dead == False:
+                    self.score += 10
+                    item.Dead = True
+                    print(item.Dead)
+                item.remove_from_sprite_lists()
+        if (gosling_time) > 60:
+            gosling_time = 0
+            self.coin1 = Gosling(self.num)
+            g += 3
+            C -= 0.0001
+            self.coin_list.append(self.coin1)
+            self.sprite_list.append(self.coin1)
+
+
+
+
+class Svin(arcade.Sprite):
+    change_x = 0
     def __init__(self):
-        self.transp = False
-        self.x_pos = 150
-        self.y_pos = 700
-        self.edet_right = False
-        self.edet_left = False
-        self.img = Image.open("Svin.png")
-        self.small_Svin = self.img.resize((300, 200))
-        self.Tk_small_Svin = ImageTk.PhotoImage(self.small_Svin)
-        self.svin_obj = simulation.create_image(self.x_pos, self.y_pos, image=self.Tk_small_Svin)
+        self.right1 = False
+        self.left1 = False
+        super().__init__("Svin.png", scale=1)
+        self.original_texture = self.texture
+        self.flipped_texture = self.texture.flip_left_right()
+        self.center_x = 150
+        self.center_y = 100
 
-    def rotate(self, a):
-        if a == 0 and self.edet_right == True:
-            self.img = Image.open("Svin.png")
-            self.small_Svin = self.img.resize((300, 200))
-            self.Tk_small_Svin = ImageTk.PhotoImage(self.small_Svin)
-            self.svin_obj = simulation.create_image(self.x_pos, self.y_pos, image=self.Tk_small_Svin)
-        if a == 1 and self.edet_left == True:
-            self.img = self.img.transpose(Image.FLIP_LEFT_RIGHT)
-            self.small_Svin = self.img.resize((300, 200))
-            self.Tk_small_Svin = ImageTk.PhotoImage(self.small_Svin)
-            self.svin_obj = simulation.create_image(self.x_pos, self.y_pos, image=self.Tk_small_Svin)
+    def on_key_press(self,key):
+        if key == arcade.key.D:
+            self.change_x = 5
+            self.right1 = True
+            self.texture = self.original_texture
+        if key == arcade.key.A:
+            self.change_x = -5
+            self.left1 = True
+            self.texture = self.flipped_texture
 
-    def start_move_right(self, event):
-        if self.edet_right == False:
-            print("начинаем")
-            self.edet_right = True
-            self.move_right()
-            self.rotate(0)
+    def on_key_release(self, key):
+        if key == arcade.key.D and self.change_x == 5:
+            self.change_x = 0
+            self.right1 = False
+        if key == arcade.key.A and self.change_x == -5:
+            self.change_x = 0
+            self.left1 = False
 
-    def end_move_right(self, event):
-        print("кончаем")
-        self.edet_right = False
-
-    def move_right(self):
-
-        if self.edet_right == True:
-            self.x_pos += 10
-            simulation.coords(self.svin_obj, self.x_pos, self.y_pos)
-            root.after(20, lambda : self.move_right())
-
-    def start_move_left(self, event):
-        if self.edet_left == False:
-            self.orient = 1
-            self.edet_left = True
-            self.rotate(1)
-            self.move_left()
-
-    def end_move_left(self, event):
-        self.edet_left = False
-
-    def move_left(self):
-        if self.edet_left == True:
-            self.x_pos -= 10
-            simulation.coords(self.svin_obj, self.x_pos, self.y_pos)
-            root.after(20, lambda : self.move_left())
+    def update(self):
+        self.center_x += self.change_x
 
 
 def f(t, v):
     return -g-C*v*abs(v)
 
-def move_circle( index ):
-    global y_pos
-    global i
-    global m
-    if index < len(t)-1:
-        while i < index:
-            k_1 = f(t[i], v[i])
-            k_2 = f(t[i] + dt / 2, v[i] + k_1 * dt / 2)
-            k_3 = f(t[i] + dt / 2, v[i] + k_2 * dt / 2)
-            k_4 = f(t[i] + dt, v[i] + k_3 * dt)
-            v[i + 1] = v[i] + (dt / 6) * (k_1 + 2 * k_2 + 2 * k_3 + k_4)
-            y[i + 1] = y[i] + dt * v[i]
-            i += 1
-            #print (i)
-        y_pos = -y[index] + 550
-        if y_pos < 760:
-            simulation.coords(gosling_obj, 50, y_pos)
-        t_int = int(dt*1000)
-        now = time.perf_counter()
-        delta = now - start_time
-        if delta > 5 and m == 0:
-            m = 1
-            print (delta, " ", index)
-        index = int(100*delta)
+class Gosling(arcade.Sprite):
 
-        root.after(t_int, lambda : move_circle(index+1))
+    def __init__ (self, n):
+        super().__init__("Gosling.png", scale=1)
+        self.start_time = time.perf_counter()
+        self.center_x = random.randint(0, 1200)
+        self.center_y = 800
+        self.index = 0
+        self.i = 0
+        self.num = n
+        self.Dead = False
 
 
-def key_check(event):
-    print(f"Нажата: {event.char}")
+
+    def move_coin(self):
+        if self.index < len(t)-1:
+            while self.i < self.index:
+                k_1 = f(t[self.i], v[self.i])
+                k_2 = f(t[self.i] + dt / 2, v[self.i] + k_1 * dt / 2)
+                k_3 = f(t[self.i] + dt / 2, v[self.i] + k_2 * dt / 2)
+                k_4 = f(t[self.i] + dt, v[self.i] + k_3 * dt)
+                v[self.i + 1] = v[self.i] + (dt / 6) * (k_1 + 2 * k_2 + 2 * k_3 + k_4)
+                y[self.i + 1] = y[self.i] + dt * v[self.i]
+                self.i += 1
+                #print (i)
+            if self.center_y > 41:
+                 self.center_y = y[self.index] + 250
+            now = time.perf_counter()
+            delta = now - self.start_time
+            self.index = int(60*delta)
+
+    def update(self):
+        self.move_coin()
+
 
 print(t[500])
 print(v[1])
 
-root = tk.Tk()
-root.title("Каки падают на асфальт")
-root.geometry("1200x800")
-simulation = tk.Canvas(bg = "white",width = 1200, height = 800)
-simulation.pack(anchor= tk.CENTER, expand=1)
-r = 20
-
-
-gosling_landscape = Image.open("Landscape.png")
-small_landscape = gosling_landscape.resize((1200,800))
-Tk_small_landscape = ImageTk.PhotoImage(small_landscape)
-landscape_obj = simulation.create_image(600, 400, image = Tk_small_landscape)
-gosling_ball = Image.open("Gosling.png")
-small_gosling_ball = gosling_ball.resize((80,80))
-Tk_small_gosling_ball = ImageTk.PhotoImage(small_gosling_ball)
-gosling_obj = simulation.create_image(100, 100, image = Tk_small_gosling_ball)
-
-start_time = time.perf_counter()
-move_circle(0)
-Car1 = Svin()
-root.bind("<Key>", key_check)
-root.bind("<KeyPress-D>", Car1.start_move_right)
-root.bind("<KeyRelease-D>", Car1.end_move_right)
-root.bind("<KeyPress-d>", Car1.start_move_right)
-root.bind("<KeyRelease-d>", Car1.end_move_right)
-root.bind("<KeyPress-A>", Car1.start_move_left)
-root.bind("<KeyRelease-A>", Car1.end_move_left)
-root.bind("<KeyPress-a>", Car1.start_move_left)
-root.bind("<KeyRelease-a>", Car1.end_move_left)
 
 pygame.mixer.init()
 pygame.mixer.music.load("Nightcall.mp3")
 pygame.mixer.music.play(-1)
-root.focus_set()
-root.mainloop()
+
+game = GG(1200, 800)
+
+arcade.run()
 print("Hello world")
