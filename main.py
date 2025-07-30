0cc8d63faecbfd542dc8d1008230c303d3b3affc
BohYapoh <seregin.petr0705@gmail.com>
2025-07-30 21:49:11 +0300

    Gosling

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ class GG (arcade.Window):
     def on_draw(self):
         #print(type(self.background))
         self.sprite_list.draw()
-        arcade.draw_text(self.score, x = 100, y =200 )
+        arcade.draw_text(f"Score : {self.score}", x = 20, y =760, font_size = 25, color = arcade.color.PURPLE)
 
     def on_update(self, delta_time):
         global C
@@ -83,8 +83,7 @@ class GG (arcade.Window):
         if (gosling_time) > 60:
             gosling_time = 0
             self.coin1 = Gosling(self.num)
-            g += 3
-            C -= 0.0001
+            g += 1
             self.coin_list.append(self.coin1)
             self.sprite_list.append(self.coin1)
 
@@ -101,27 +100,60 @@ class Svin(arcade.Sprite):
         self.flipped_texture = self.texture.flip_left_right()
         self.center_x = 150
         self.center_y = 100
+        self.Power = 14700
+        self.E_kin = 0
+        self.mass = 300
+        self.velocity = 0
+        self.dir = 0
+        self.g = 9.81
+        self.desired_dir = 0
 
     def on_key_press(self,key):
         if key == arcade.key.D:
-            self.change_x = 5
+            #self.change_x = 5
             self.right1 = True
-            self.texture = self.original_texture
-        if key == arcade.key.A:
-            self.change_x = -5
+            self.desired_dir = 1
+        elif key == arcade.key.A:
+            #self.change_x = -5
+            self.desired_dir = -1
             self.left1 = True
-            self.texture = self.flipped_texture
+        else:
+            self.right1 = False
+            self.left1 = False
+
 
     def on_key_release(self, key):
-        if key == arcade.key.D and self.change_x == 5:
+        if key == arcade.key.D:
             self.change_x = 0
             self.right1 = False
-        if key == arcade.key.A and self.change_x == -5:
+        if key == arcade.key.A:
             self.change_x = 0
             self.left1 = False
 
     def update(self):
-        self.center_x += self.change_x
+        global C
+        if self.desired_dir == 1 and self.velocity == 0:
+            self.dir = 1
+            self.texture = self.original_texture
+        if self.desired_dir == -1 and self.velocity == 0:
+            self.dir = -1
+            self.texture = self.flipped_texture
+        if (self.right1 == True or self.left1 == True) and self.dir==self.desired_dir:
+            self.E_kin += self.Power * (1 / 60)
+        if (self.right1 == True or self.left1 == True) and self.dir!=self.desired_dir:
+            if abs(self.E_kin) > abs(self.Power * (1 / 60)):
+                self.E_kin -= self.Power * (1/60)
+            else:
+                self.E_kin = 0
+        self.A_loss = (C * abs(self.velocity ** 3) + 0.3 * self.mass * self.g * abs(self.velocity)) * (1 / 60)
+        if abs(self.E_kin) > abs(self.A_loss):
+           self.E_kin -= self.A_loss
+        else:
+            self.E_kin = 0
+        self.velocity = self.dir * math.sqrt(2 * self.E_kin / self.mass)
+        #self.velocity = math.sqrt(2*self.E_kin/self.mass)
+        self.center_x += self.velocity/0.6
+        #print(self.E_kin, " ", self.velocity)
 
 
 def f(t, v):
@@ -157,6 +189,7 @@ class Gosling(arcade.Sprite):
             now = time.perf_counter()
             delta = now - self.start_time
             self.index = int(60*delta)
+            #print(delta," ", self.index)
 
     def update(self):
         self.move_coin()
